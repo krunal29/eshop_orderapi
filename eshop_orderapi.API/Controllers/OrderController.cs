0874dd18cce066352dd6702b3a6e7d62c9aa8563
jagdish.karmur <jagdish.karmur@ifour.com>
2024-10-10 16:09:52 +0530

    cgabges

--- eshop_orderapi.API/Controllers/OrderController.cs
+++ eshop_orderapi.API/Controllers/OrderController.cs
@@ -86,6 +86,27 @@ namespace eshop_orderapi.API.Controllers
                     return Response(new BooleanResponseModel { Value = flag }, _localizer["RecordDeleteSuccess"].Value.ToString());
                 return Response(new BooleanResponseModel { Value = flag }, _localizer["ReordNotDeleteSucess"].Value.ToString(), DropMessageType.Error);
             });
+            return await GetDataWithMessage(async () =>
+            {
+                var flag = await _OrderService.DeleteAsync(id);
+                if (flag)
+                    return Response(new BooleanResponseModel { Value = flag }, _localizer["RecordDeleteSuccess"].Value.ToString());
+                return Response(new BooleanResponseModel { Value = flag }, _localizer["ReordNotDeleteSucess"].Value.ToString(), DropMessageType.Error);
+            });
+            return await GetDataWithMessage(async () =>
+            {
+                var flag = await _OrderService.DeleteAsync(id);
+                if (flag)
+                    return Response(new BooleanResponseModel { Value = flag }, _localizer["RecordDeleteSuccess"].Value.ToString());
+                return Response(new BooleanResponseModel { Value = flag }, _localizer["ReordNotDeleteSucess"].Value.ToString(), DropMessageType.Error);
+            });
+            return await GetDataWithMessage(async () =>
+            {
+                var flag = await _OrderService.DeleteAsync(id);
+                if (flag)
+                    return Response(new BooleanResponseModel { Value = flag }, _localizer["RecordDeleteSuccess"].Value.ToString());
+                return Response(new BooleanResponseModel { Value = flag }, _localizer["ReordNotDeleteSucess"].Value.ToString(), DropMessageType.Error);
+            });
         }
 
         [HttpDelete]
